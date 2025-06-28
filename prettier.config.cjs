95b7a7e16081a4e720a84eb3583a532fe238a146
wwcoderww <wwcoderww@gmail.com>
2025-06-27 21:48:54 -0500

    Installed TailwindCSS

---
+++ prettier.config.cjs
@@ -0,0 +1,11 @@
+// prettier.config.js, .prettierrc.js, prettier.config.cjs, or .prettierrc.cjs
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  plugins: ["prettier-plugin-tailwindcss"],
+};
+
+module.exports = config;
